befe2d920beb73daf17c59e3ebf5635b640bd833
Benjamin Silva <benjamin.silva@insight4.tech>
2023-03-06 21:00:49 -0500

    set port with var environment

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const app = express();
+const port = process.env.PORT || 3000
 
-app.get('/', (req, res) => {
+app.get('/dashboard', (req, res) => {
   const data = {
     value1: 1,
     value2: 2,
@@ -12,6 +13,6 @@ app.get('/', (req, res) => {
   res.json(data);
 });
 
-app.listen(3000, () => {
-  console.log('Server running on port 3000');
+app.listen( port, () => {
+  console.log(`Server running on port ${port} or 3000`);
 });
